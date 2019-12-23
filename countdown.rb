238d7857fab62ffec8c3808785a51f7998669dcf
Doak Covington <doakcovington@protonmail.com>
2019-12-23 00:06:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown (int)
+  seconds =  int
+  while seconds > 0
+    puts "#{int} SECOND(S)!"
+    seconds -=
+  end
+  return "HAPPY NEW YEAR!"
 end

a076350de223fafebe44c4119bc185df2e393e99
Doak Covington <doakcovington@protonmail.com>
2019-12-23 00:14:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,7 +13,7 @@ def countdown_with_sleep(int)
   seconds = int
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
-    sleep(1.second)
+    sleep(1)
     seconds -= 1
   end
   return "HAPPY NEW YEAR!"
